4da35624075cbef476882daed135b98915cb4f9d
jaschamerle <jaschadevelopment@gmail.com>
2025-02-15 12:27:51 +0100

    fix(workflows/push): set-dotnet-version-for-tests.sh script now properly uses passed parameter

--- .github/workflows/scripts/push/set-dotnet-version-for-tests.sh
+++ .github/workflows/scripts/push/set-dotnet-version-for-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Convert the current matrix target framework (e.g. net6.0) to an SDK version (e.g. 6.0.x)
-sdk_version=$(echo "${{ matrix.dotnet_version }}" | sed 's/^net//').x
+sdk_version=$(echo "${1}" | sed 's/^net//').x
 echo "Calculated .NET SDK version for test: $sdk_version"
-echo "sdk_version=$sdk_version" >> $GITHUB_OUTPUT
+echo "sdk_version=$sdk_version" >> "$GITHUB_OUTPUT"
