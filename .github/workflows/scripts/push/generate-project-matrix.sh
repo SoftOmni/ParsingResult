6b49918064671e03355508ed7ffe29d68ad57a15
jaschamerle <jaschadevelopment@gmail.com>
2025-02-16 04:42:51 +0100

    fix(workflows/push): .net version matrix patched to work with external file

--- .github/workflows/scripts/push/generate-project-matrix.sh
+++ .github/workflows/scripts/push/generate-project-matrix.sh
@@ -6,7 +6,7 @@ test_projects=$(find "${1}" -type f -name "*Tests.csproj")
 echo "Found projects: $test_projects"
 
 # Extract .NET versions from the earlier matrix (assumes it's a JSON object with an 'include' key)
-versions=$(echo '${{ needs.generate-matrix.outputs.matrix }}' | jq -r '.include[].dotnet_version')
+versions=$(echo "${2}" | jq -r '.include[].dotnet_version')
 
 # Build a JSON array of matrix entries.
 matrix_entries="[]"
